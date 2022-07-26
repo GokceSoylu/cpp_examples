9bae48c3d57c8302519e132bcb2a99c6aa2e77fe
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-07-26 07:46:19 +0300

    .

---
+++ classFunc.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class Try
+{
+    public:
+        int x ,y;
+        void atama(int x_, int y_)
+        {
+            x=x_;
+            y=y_;
+        }
+        void print()
+        {
+            cout<<endl<<x<<" "<<y;
+        }
+        bool control()
+        {
+            if(x%2==0 && y%2==0)
+                return true;
+            else
+                return false;
+        }
+};
+int main()
+{
+    Try T1, T2;//bunlar nesne
+    T1.atama(2,4);
+    T2.atama(6,46);
+    if(T1.control() && T2.control())
+    {
+        T1.print();
+        T2.print();
+    }
+}
