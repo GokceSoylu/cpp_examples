9aa5f03b0b6201e49b09a8ffcddb96aa952a0b6f
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-07-28 11:23:32 +0300

    .

---
+++ class_copyConstructer.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class Numbers
+{
+    int x, y, *p;
+    int const z;
+    public:
+        Numbers() : z(3)
+        {
+            x=y=3;
+            p = new int;
+            *p=3;
+        }
+        Numbers(int x, int y, int z) : z(z)
+        {
+            this->x=x;
+            this->y=y;
+            p=new int;
+            *p=z;
+        }
+        Numbers(Numbers &n) : z(n.z)
+        {
+            x=n.x;
+            y=n.y;
+            p=new int;
+            p=n.p;
+        }
+        void get()
+        {
+            cout<<"\nx = "<<x<<"\ny = "<<y<<"\nz = "<<z<<"\n*p = "<<*p;
+        }
+};
+int main()
+{
+    Numbers n(1,2,3), n2(n);
+    n.get();
+    n2.get();
+}
+//copy constructer adı üstünde kopyalayan kurucu fonksiyon. Sınıfımızın kendi türünden bir değişken veririz aynı içerikte
+//yeni değişken oluşturur:)
