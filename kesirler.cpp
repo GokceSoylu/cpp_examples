b562837bb7b6ffead54765d1f86d6299cfaf543c
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-07-31 01:21:54 +0300

    .

--- kesirler.cpp
+++ kesirler.cpp
@@ -3,36 +3,23 @@ void count(int counter, int i, int *x, int *y);
 int basamak(int);
 int main()
 {
-    int i, x, y, counter500=0, counter1000=0, counter1500=0;
-    for(i=1;i<=100;i++)
+    int i, x, y, counter100=0, counter500=0, counter1000=0, counter1500=0;
+    for(i=1;i<=1500;i++)
     {
         x=3;
         y=2;
         count(1,i,&x,&y);
+        //std::cout<<std::endl<<" "<<i<<" "<<x<<" "<<y;
         if(basamak(x)>basamak(y)) 
         {
-            counter500++;
+            if(i<=100) counter100++;
+            else if(i<=500) counter500++;
+            else if(i<=1000) counter1000++;
+            else if(i<=15000) counter1500++;
             //std::cout<<std::endl<<" "<<i<<" "<<x<<" "<<y;
         }
-            
     }
-    for(i=501;i<=1500;i++)
-    {
-        x=3;
-        y=2;
-        count(1,i,&x,&y);
-        if(basamak(x)>basamak(y))
-            counter1000++;
-    }
-    for(i=1001;i<=1500;i++)
-    {
-        x=3;
-        y=2;
-        count(1,i,&x,&y);
-        if(basamak(x)>basamak(y))
-            counter1500++;
-    }
-    std::cout<<"\nilk 500'de "<<counter500<<"\nilk 1000'de "<<counter1000<<"\nilk 1500'de "<<counter1500;
+        std::cout<<"\nilk 100'de "<<counter100<<"\nilk 500'de "<<counter500<<"\nilk 1000'de "<<counter1000<<"\nilk 1500'de "<<counter1500;
 }
 void count(int counter, int i, int *x, int *y)
 {
