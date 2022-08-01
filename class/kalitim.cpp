2bf1f35ca5fe3e21549b387b933d9c6cb767ee1c
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-08-01 14:28:08 +0300

    .

--- class/kalitim.cpp
+++ class/kalitim.cpp
@@ -3,25 +3,8 @@
 using namespace std;
 class cokgen
 {
-    public:
+    protected:
         int h, l;
-    public:
-    class ucgen//: public cokgen
-    {
-        /*protected:
-        int h, l;
-        public:
-        void atama(int h_input, int l_input)
-        {
-            h=h_input;
-            l=l_input;
-        }
-        */public:
-            float alan()
-            {
-                return (h*l*0.5);
-            }
-    };
     public:
         void atama(int h_input, int l_input)
         {
@@ -29,10 +12,16 @@ class cokgen
             l=l_input;
         }
 };
-
+class ucgen: public cokgen
+{
+    public:
+        float alan()
+        {
+            return (h*l*0.5);
+        }
+};
 class dikdortgen: public cokgen
 {
-
     public:
         float alan()
         {
@@ -50,7 +39,7 @@ class silindir: public cokgen
 int main()
 {
     dikdortgen dik;
-    cokgen::ucgen uc;
+    ucgen uc;
     silindir sil;
     dik.atama(3,4);
     uc.atama(3,4);
--- kesirler.cpp
+++ kesirler.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-void count(int counter, int i, int *x, int *y);
-int basamak(int);
+void count(int counter, int i, long int *x, long int *y);
+int basamak(long int);
 int main()
 {
-    int i, x, y, counter100=0, counter500=0, counter1000=0, counter1500=0;
+    int i, counter100=0, counter500=0, counter1000=0, counter1500=0;
+    long int x, y; 
     for(i=1;i<=1500;i++)
     {
         x=3;
@@ -16,14 +17,14 @@ int main()
             else if(i<=500) counter500++;
             else if(i<=1000) counter1000++;
             else if(i<=15000) counter1500++;
-            //std::cout<<std::endl<<" "<<i<<" "<<x<<" "<<y;
+            std::cout<<std::endl<<" "<<i<<" "<<x<<" "<<y;
         }
     }
     std::cout<<"\nilk 100'de "<<counter100<<"\nilk 500'de "<<counter500<<"\nilk 1000'de "<<counter1000<<"\nilk 1500'de "<<counter1500;
 }
-void count(int counter, int i, int *x, int *y)
+void count(int counter, int i, long  int *x, long int *y)
 {
-    int tmp;
+    long int tmp;
     if(counter==i)
     {
         return;
@@ -37,7 +38,7 @@ void count(int counter, int i, int *x, int *y)
         return count(counter+1,i,x,y);
     }
 }
-int basamak(int n)
+int basamak(long int n)
 {
     int i=0;
     while(n!=0)
